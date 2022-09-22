c9f9cb345059183800e169b9a6f2de28953d7255
Fraser <frasernz@gmail.com>
2022-09-22 11:25:16 -0500

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,11 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Media Central',
   tagline: 'Documentation',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://www.mediacentral.dev/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  // organizationName: 'facebook', // Usually your GitHub org/user name.
-  // projectName: 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -64,7 +59,7 @@ const config = {
             label: 'All Tags',
           },
           {
-            href: 'https://github.com/MediaCentral/mediacentral-docs',
+            href: 'https://github.com/MediaCentral/mediacentral.dev',
             label: 'GitHub',
             position: 'right',
           },
@@ -109,7 +104,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Media Central. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
